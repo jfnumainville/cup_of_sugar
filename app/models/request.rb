fedc64206136b7d8105e3a65659e30644aab59f3
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-05-21 22:44:44 -0400

    DB Fix

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,8 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  has_many :reviews
+
+  validates :start_date, :end_date, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  validates :rating, :description, presence: true
 end
