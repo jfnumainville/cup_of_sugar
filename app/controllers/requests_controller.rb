a8e84e8587cfd9334e34a684238e32c111b470e7
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-05-26 21:17:15 -0400

    Routes done

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,24 @@
+class RequestsController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+
+end
