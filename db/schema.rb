d7e9cc79f14ac9558d2e11ad74fef1069a24bb62
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-05-28 15:27:25 -0400

    m

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_151144) do
+ActiveRecord::Schema.define(version: 2022_05_28_161306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2022_05_28_151144) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
     t.string "name"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
