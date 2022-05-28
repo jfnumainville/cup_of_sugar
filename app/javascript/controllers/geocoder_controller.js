7421ccabbebb462429b7179d0a474240e621c6c6
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-05-28 16:56:18 -0400

    Map is done

--- app/javascript/controllers/geocoder_controller.js
+++ app/javascript/controllers/geocoder_controller.js
@@ -1,13 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
+
 export default class extends Controller {
   static values = { apiKey: String }
 
   static targets = ["address"]
 
   connect() {
-      console.log("Hello")
+      console.log("Hello 2")
     this.geocoder = new MapboxGeocoder({
       accessToken: this.apiKeyValue,
       types: "country,region,place,postcode,locality,neighborhood,address"
--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -9,44 +9,56 @@ export default class extends Controller {
   }
 
   connect() {
-      console.log('Hello');
+      console.log('Hello 1');
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb"
     });
+
+
+    // this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+    //     mapboxgl: mapboxgl }))
+
     this._addMarkersToMap()
     this._fitMapToMarkers()
-
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl }))
   }
 
+
   _addMarkersToMap() {
+    // this.markersValue.forEach((marker) => {
+    //     const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+    //     console.log(marker);
+
+    //     // Create a HTML element for your custom marker
+    //     const customMarker = document.createElement("div")
+    //     customMarker.className = "marker"
+    //     customMarker.style.backgroundImage = `url('${marker.image_url}')`
+    //     customMarker.style.backgroundSize = "contain"
+    //     customMarker.style.width = "25px"
+    //     customMarker.style.height = "25px"
+
+    //     // Pass the element as an argument to the new marker
+    //     new mapboxgl.Marker(customMarker)
+    //       .setLngLat([marker.lng, marker.lat])
+    //       .setPopup(popup)
+    //       .addTo(this.map)
+    //   })
+
     this.markersValue.forEach((marker) => {
         const popup = new mapboxgl.Popup().setHTML(marker.info_window)
-
-        // Create a HTML element for your custom marker
-        const customMarker = document.createElement("div")
-        customMarker.className = "marker"
-        customMarker.style.backgroundImage = `url('${marker.image_url}')`
-        customMarker.style.backgroundSize = "contain"
-        customMarker.style.width = "25px"
-        customMarker.style.height = "25px"
-
-        // Pass the element as an argument to the new marker
-        new mapboxgl.Marker(customMarker)
-          .setLngLat([marker.lng, marker.lat])
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
           .setPopup(popup)
           .addTo(this.map)
-      })
+      });
   }
 
   _fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 10, duration: 0 })
   }
 
 }
