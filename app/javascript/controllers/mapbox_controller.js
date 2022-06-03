3ac92fd445a8af030e7ef29171ce76c0e441ed86
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-06-03 00:55:37 -0400

    Photo fixed but need improvements to seedFile

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -58,7 +58,7 @@ export default class extends Controller {
   _fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 10, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 11, duration: 0 })
   }
 
 }
