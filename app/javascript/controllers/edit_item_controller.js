dff6ee2d5016cdb179e61d10bb6d0044ee31cc0c
Suzanne Tordjman <suzannetor@gmail.com>
2022-06-04 12:03:21 -0400

    rating heart => need to find something else, update bug resolved

--- app/javascript/controllers/edit_item_controller.js
+++ app/javascript/controllers/edit_item_controller.js
@@ -4,12 +4,12 @@ export default class extends Controller {
   static targets = [ 'infos', 'form', 'card' ];
 
   displayForm() {
-    console.log("displaying")
     this.infosTarget.classList.add("d-none");
     this.formTarget.classList.remove("d-none");
   }
 
   update(event) {
+    console.log("something new")
     event.preventDefault()
     const url = this.formTarget.action
     fetch(url, {
@@ -20,7 +20,10 @@ export default class extends Controller {
       .then(response => response.text())
       .then((data) => {
         console.log(data)
-      //  this.cardTarget.outerHTML = data
+        this.cardTarget.outerHTML = data
+        // TODO: notification popup
       })
+    this.infosTarget.classList.remove("d-none");
+    this.formTarget.classList.add("d-none");
   }
 }
