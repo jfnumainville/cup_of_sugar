91667934c42d5fecd2faa9ddcc3d9c937efec727
Mark Soosaipillai <markbrian14@hotmail.com>
2022-06-07 19:11:12 -0400

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,21 @@
 Rails.application.routes.draw do
-    root to: 'pages#home'
-    get '/landing' => 'pages#landing'
+  root to: 'pages#home'
+  get '/landing' => 'pages#landing'
 
-    devise_for :users do
-        resources :requests, only: [:index, :show]
-        resources :items
-        resources :reviews
-    end
+  devise_for :users do
+    resources :requests, only: %i[index show]
+    resources :items
+    resources :reviews
+  end
 
+  resources :items do
+    resources :requests, only: %i[new create update]
+    resources :reviews
+  end
 
-    resources :items do
-        resources :requests, only: [:new, :create, :update]
-        resources :reviews
-    end
-
-    resources :requests do
-        resources :messages, only: [:new, :create]
-    end
-
-#   patch "cancel_request/:id", to: "request#cancel", as: :cancel_request
-#   patch "approved_request/:id", to: "request#approved", as: :approved_request
-
+  resources :requests do
+    resources :messages, only: :create
+  end
+  #   patch "cancel_request/:id", to: "request#cancel", as: :cancel_request
+  #   patch "approved_request/:id", to: "request#approved", as: :approved_request
 end
