25bc600b87560ed42c76764c84ad9dbefdce48f8
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-06-08 15:27:38 -0400

    Click on chat button on item/show page opens a new chat window

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,16 @@
 class ChatroomsController < ApplicationController
+  def create
+    @item = Item.find(params[:id])
+    @chatroom = Chatroom.new(name: @item.name)
+
+    if @chatroom.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      redirect_to item_path(@item)
+    end
+  end
+
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   resources :requests
 
-  resources :chatrooms, only: :show do
+  resources :chatrooms, only: %i[create show] do
     resources :messages, only: :create
   end
 
