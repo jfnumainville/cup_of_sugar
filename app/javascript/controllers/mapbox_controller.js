b6f15382a48bf9ae13e09101f31d379ab37b1e5d
Suzanne Tordjman <suzannetor@gmail.com>
2022-06-08 18:31:08 -0400

    show page, homepage, dashboard

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -29,8 +29,8 @@ export default class extends Controller {
         customMarker.className = "marker";
         customMarker.style.backgroundImage = `url('${marker.image_url}')`;
         customMarker.style.backgroundSize = "contain";
-        customMarker.style.width = "25px";
-        customMarker.style.height = "25px";
+        customMarker.style.width = "40px";
+        customMarker.style.height = "40px";
 
         // Pass the element as an argument to the new marker
         new mapboxgl.Marker(customMarker)
