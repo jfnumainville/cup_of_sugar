0433915872248b058677bea6a9752ec35de08ffc
Suzanne Tordjman <suzannetor@gmail.com>
2022-06-07 21:31:29 -0400

    changes done this evening

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -39,19 +39,12 @@ export default class extends Controller {
           .addTo(this.map)
       })
 
-    this.markersValue.forEach((marker) => {
-        const popup = new mapboxgl.Popup().setHTML(marker.info_window)
-        new mapboxgl.Marker()
-          .setLngLat([ marker.lng, marker.lat ])
-          .setPopup(popup)
-          .addTo(this.map)
-      });
   }
 
   _fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 25, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 20, duration: 0 })
   }
 
 }
