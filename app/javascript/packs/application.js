153c572ebf2027c2c4fa3b5394c922ca23675211
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-06-10 00:06:49 -0400

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,70 +9,14 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "controllers"
 import "bootstrap"
-import { initSweetalert } from '../plugins/init_sweetalert';
 import { initStarRating } from '../plugins/init_star_rating';
-
-initStarRating();
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-
-
-initSweetalert('#sweet-alert-delete', {
-  title: "Are you sure?",
-  text: "This action cannot be reversed",
-  icon: "warning"
-}, (value) => {
-  console.log("We got: ",value)
-  if (value) {
-    const link = document.querySelector('#delete-link');
-    link.click();
-  }
-}
-
-);
-
-initSweetalert('#sweet-alert-create', {
-  title: "Thank you !!!",
-  text: "Your item has been added to Cup of Sugar",
-  icon: "success"
-}, (value) => {
-  if (value) {
-    const submit = document.querySelector('#create-link');
-    form.submit();
-  }
-}
-
-);
-
-initSweetalert('#sweet-alert-update', {
-  title: "Hello !",
-  text: "Your item has been updated.",
-  icon: "success"
-}, (value) => {
-  if (value) {
-    const link = document.querySelector('#update-link');
-    link.click();
-  }
-}
-
-);
-
-initSweetalert('#sweet-alert-reservation', {
-  title: "Thank you !",
-  text: "Your reservation has been sent",
-  icon: "success"
-}, (value) => {
-  if (value) {
-    const link = document.querySelector('#reservation-link');
-    link.click();
-  }
-}
-
-);
 document.addEventListener('turbolinks:load', () => {
     // Call your functions here, e.g:
     // initSelect2();
+    initStarRating();
   });
