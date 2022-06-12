2825d14a78fd535c368f4c5e5d544af547e8de2e
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-06-12 14:05:21 -0400

    TypedJS done#version2

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,7 +4,7 @@ import Typed from "typed.js";
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-        strings: ['Why <strong><i>BUY</i> or <i>RENT</i></strong> when you can <strong><i>BORROW</i></strong> from your neighbours?'],
+        strings: ['Why <strong><i>BUY</i> or <i>RENT</i></strong> when you can <strong><i>BORROW</i></strong>&nbsp; from your neighbours?'],
         typeSpeed: 70,
         backSpeed: 90,
         loop: true,
