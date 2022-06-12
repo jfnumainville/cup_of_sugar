c9af244b444002e20b4d40b58fa9ad7fff0bddb4
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-06-12 13:35:26 -0400

    TypedJS done

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus";
+import Typed from "typed.js";
+
+export default class extends Controller {
+  connect() {
+    new Typed(this.element, {
+        strings: ['Why <strong><i>BUY</i> or <i>RENT</i></strong>', 'When you can <strong><i>BORROW</i></strong>'],
+        typeSpeed: 70,
+        backSpeed: 90,
+        loop: true,
+        loopCount: Infinity,
+        showCursor: false
+    });
+  }
+}
