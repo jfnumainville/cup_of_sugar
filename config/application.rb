8f68ce79d580f531a84eee8d08aec1b79dc3e7ef
Mark Soosaipillai <markbrian14@hotmail.com>
2022-06-14 13:15:15 -0400

    fixed notification

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module CupOfSugar
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.exceptions_app = self.routes
-
+    config.action_controller.default_protect_from_forgery = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
