ef3466f8d9375ab4ef35da63816985837bebb649
Mark Soosaipillai <markbrian14@hotmail.com>
2022-06-16 16:54:33 -0400

    fixed line break in typed-js

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,7 +4,7 @@ import Typed from "typed.js";
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-        strings: ['<h1 class = "display-landing">Why <strong><i>BUY</i> or <i>RENT</i></strong> when you can <strong><i>BORROW</i></strong>&nbsp; from your neighbours?</h1>'],
+        strings: ['<h1 class = "display-landing">Why <strong><i>BUY</i> or <i>RENT</i></strong><br> when you can <strong><i>BORROW</i></strong>&nbsp; from your neighbours?</h1>'],
         typeSpeed: 70,
         backSpeed: 90,
         loop: true,
